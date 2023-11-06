3008afa425f9d0f85d380f58fc4d98d2a3133360
OctavioM3 <octaviom3@nycstudents.net>
2023-11-06 09:24:35 -0500

    dasdffsfsaf

--- dom.js
+++ dom.js
@@ -1,35 +1,35 @@
 const DOMSelectors = {
-    box: document.getElementById("firstbox"),
-    NameInput: document.querySelector(".name-input"),
-    formName: document.querySelector("#formName"),
+  box: document.getElementById("firstbox"),
+  NameInput: document.querySelector(".name-input"),
+  formName: document.querySelector("#formName"),
 };
 
 DOMSelectors.formName.addEventListener("submit", function (event) {
-    event.preventDefault()
-    enter()
-    clear()
-    clearInputspace()
+  event.preventDefault();
+  enter();
+  clear();
+  clearInputspace();
 });
 
 function enter() {
-    let input = DOMSelectors.NameInput.value;
-    DOMSelectors.box.insertAdjacentHTML("beforeend", `<div><ul><li> ${input} </li></ul>
-    <button id="clear">Clear</button></div>`);
-};
+  let input = DOMSelectors.NameInput.value;
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<div><ul><li> ${input}
+    <button id="clear">Clear</button></li></ul></div>`
+  );
+  console.log(input);
+}
 
 function clear() {
-    let remove = document.querySelectorAll("#clear")
-    remove.forEach((erase) => {
-        erase.addEventListener("click", function(secondevent){
-            secondevent.target.parentElement.remove()
-        });
+  let remove = document.querySelectorAll("#clear");
+  remove.forEach((erase) => {
+    erase.addEventListener("click", function (secondevent) {
+      secondevent.target.parentElement.remove();
     });
-};
+  });
+}
 
 function clearInputspace() {
-    DOMSelectors.NameInput.value = "";
-};
-
-
-
-
+  DOMSelectors.NameInput.value = "";
+}
